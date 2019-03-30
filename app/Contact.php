a34b13c13d01dec1bfb5363bb06219139c27c239
Mederic Roy Beldia <twocngdagz@yahoo.com>
2019-03-30 15:34:27 +0800

    add trait

--- app/Contact.php
+++ app/Contact.php
@@ -2,12 +2,14 @@
 
 namespace App;
 
+use App\ContactInfo;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Contact extends Model
 {
     use SoftDeletes;
+    use ContactInfo;
 
     protected $fillable = ['name','email','phone','country','city','state','zip'];
 }
