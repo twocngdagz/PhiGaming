73abf5294f4934387a5a83c8847938aec468e4b9
Mederic Roy Beldia <twocngdagz@yahoo.com>
2019-03-30 13:41:32 +0800

    add contact model and migration

---
+++ database/migrations/2019_03_30_051931_create_information_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInformationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->string('email');
+            $table->string('phone');
+            $table->string('country');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zip');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
