87dfea0b12b177e1272aa2261242d9acccadfb9f
Mederic Roy Beldia <twocngdagz@yahoo.com>
2019-03-30 14:25:31 +0800

    add create function

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Contact;
 use App\Http\Requests\ContactRequest;
 
 
@@ -9,6 +10,14 @@ class ContactController extends Controller
 {
     public function store(ContactRequest $request)
     {
-
+        return Contact::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'phone' => $request->input('phone'),
+            'country' => $request->input('country'),
+            'city' => $request->input('city'),
+            'state' => $request->input('state'),
+            'zip' => $request->input('zip'),
+        ]);
     }
 }
