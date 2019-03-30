887b1e012a9f8c52b27569c6b51813acc31f0e14
Mederic Roy Beldia <twocngdagz@yahoo.com>
2019-03-30 15:35:44 +0800

    add contact form

---
+++ resources/assets/js/components/ContactForm.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" class="form-control" id="name" placeholder="Name">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                            <label for="name">Phone</label>
+                            <input type="text" class="form-control" id="phone" placeholder="Phone">
+                        </div>
+                        <div class="form-group">
+                            <label for="name">Country</label>
+                            <input type="text" class="form-control" id="phone" placeholder="Country">
+                        </div>
+                        <div class="form-group">
+                            <label for="name">State</label>
+                            <input type="text" class="form-control" id="phone" placeholder="State">
+                        </div>
+                        <div class="form-group">
+                            <label for="name">City</label>
+                            <input type="text" class="form-control" id="phone" placeholder="City">
+                        </div>
+                        <div class="form-group">
+                            <label for="name">Zip Code</label>
+                            <input type="text" class="form-control" id="phone" placeholder="Zip Code">
+                        </div>
+
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
