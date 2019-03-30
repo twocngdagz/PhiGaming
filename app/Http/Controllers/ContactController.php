5ef69b7bf3eccd85f855d64815ccad71bc835839
Mederic Roy Beldia <twocngdagz@yahoo.com>
2019-03-30 13:41:54 +0800

    Add contact controller and form request

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ContactRequest;
+
+
+class ContactController extends Controller
+{
+    public function store(ContactRequest $request)
+    {
+
+    }
+}
